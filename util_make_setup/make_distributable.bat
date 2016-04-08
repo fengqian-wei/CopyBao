2d46ade5bc149e7e2444990986f5ed5c4b93fe3e
KangQiao <kbridge@126.com>
2016-04-08 16:56:58 +0800

    Add readme.txt to distribution.

--- util_make_setup/make_distributable.bat
+++ util_make_setup/make_distributable.bat
@@ -13,6 +13,7 @@ copy installer.exe CopyBao\setup.exe >NUL
 copy *do.bat CopyBao\ >NUL
 copy data.zip CopyBao\ >NUL
 copy 7za.exe CopyBao\ >NUL
+copy readme.txt CopyBao\ >NUL
 echo Making target archive...
 7za a CopyBao.zip CopyBao >NUL
 echo Deleting temporary files...
